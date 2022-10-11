d6ce0834b44dce82d8309cdbf763f35b1a168eb4
Raman Kumar Bansal <64868587+ramanbansal9876@users.noreply.github.com>
2022-10-11 20:23:32 +0530

    Update tic_tac_toe.cpp

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -81,6 +81,19 @@ void display(char arr[3][3]){
 	cout<<arr[2][0]<<" | "<<arr[2][1]<<" | "<<arr[2][2]<<endl;
 }
 
+bool completelyFilled(char arr[3][3]){
+	bool res = true;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			if(arr[i][j] == '-'){
+				res = false;
+				return res;
+			}
+		}
+	}
+	return res;
+}
+
 void change(char arr[3][3],int row,int col,int turn){
 	//Takes input from the user and changes the value at that position
 	if(turn == 0){
@@ -97,59 +110,75 @@ bool filled(char arr[3][3],int row,int col){
 }
 
 int main(){
-	char arr[3][3];
-	fill_n(arr[0],3,'-');
-	fill_n(arr[1],3,'-');
-	fill_n(arr[2],3,'-');
-	int turn = 0;
-	string temp_turn = "a"; //Temporary variable to read input of turn from the user
-	//Creating this will enable us to read all the characters without any segmentation fault
+	begin: 
+		char arr[3][3];
+		fill_n(arr[0],3,'-');
+		fill_n(arr[1],3,'-');
+		fill_n(arr[2],3,'-');
+		int turn = 1;
+		string temp_turn = "a"; //Temporary variable to read input of turn from the user
+		//Creating this will enable us to read all the characters without any segmentation fault
+		
+		cout<<"Player-1 will play with \'X\' \n"; //turn = 1
+		cout<<"Player-2 will play with \'O\' \n"; //turn = 0
+		cout<<"\n";
+		while(true){
+			cout<<"Which Player will turn first (1 or 2): ";
+			getline(cin,temp_turn);
 	
-	cout<<"Player-1 will play with \'X\' \n"; //turn = 1
-	cout<<"Player-2 will play with \'O\' \n"; //turn = 0
-	cout<<"\n";
-	while(true){
-		cout<<"Which Player will turn first (1 or 2): ";
-		getline(cin,temp_turn);
-
-		if(temp_turn == "1"){
-			break;
-		}
-		else if(temp_turn == "2"){
-			turn = 0; //0 represents Player-2
-			break;
-		}
-		else{
-			cout<<"Wrong Input! Try Again\n\n";
-		}
-	}
-	
-	int row,col;
-	while(true){
-		while(true){ //Checking whether the place is already filled or not
-			cout<<"\nEnter a row number (1 or 2 or 3): ";
-			cin>>row;
-			cout<<"Enter a column number (1 or 2 or 3): ";
-			cin>>col;
-			if(row<=0 || row>=4){
-				cout<<"Invalid Row Number Input! Try Again\n";
-			}
-			else if(col<=0 || col>=4){
-				cout<<"Invalid Column Number Input! Try Again\n";
-			}
-			else if(filled(arr,row-1,col-1)){
-				cout<<"Place Already Filled! Try Again\n";
-			}
-			else{
+			if(temp_turn == "1"){
+				turn = 1; //0 represents Player-1 who plays with X
 				break;
 			}
+			else if(temp_turn == "2"){
+				turn = 0; //0 represents Player-2 who plays with O
+				break;
+			}
+			else{
+				cout<<"Wrong Input! Try Again\n\n";
+			}
 		}
-		change(arr,row-1,col-1,turn); //Passing Player Input to TIC-TAC-TOE Board
-		display(arr); //Displaying TIC-TAC-TOE Board
-		if(win_game_col(arr) || win_game_row(arr) || win_game_diag1(arr) || win_game_diag2(arr)){
-			cout<<(turn==0?"Player-2 won the game!\n":"Player-1 won the game!\n");
-			break;
+		
+		int row,col;
+		while(true){
+			while(true){ //Checking whether the place is already filled or not
+				cout<<"\nEnter a row number (1 or 2 or 3): ";
+				cin>>row;
+				cout<<"Enter a column number (1 or 2 or 3): ";
+				cin>>col;
+				if(row<=0 || row>=4){
+					cout<<"Invalid Row Number Input! Try Again\n";
+				}
+				else if(col<=0 || col>=4){
+					cout<<"Invalid Column Number Input! Try Again\n";
+				}
+				else if(filled(arr,row-1,col-1)){
+					cout<<"Place Already Filled! Try Again\n";
+				}
+				else{
+					break;
+				}
+			}
+			change(arr,row-1,col-1,turn); //Passing Player Input to TIC-TAC-TOE Board
+			display(arr); //Displaying TIC-TAC-TOE Board
+			if(win_game_col(arr) || win_game_row(arr) || win_game_diag1(arr) || win_game_diag2(arr)){
+				cout<<(turn==0?"Player-2 won the game!\n":"Player-1 won the game!\n");
+				break;
+			}
+			if(completelyFilled(arr)){
+				cout<<"\n****It's a tie!!****\n";
+				cout<<"Board has been completely filled\n";
+				break;
+			}
+			turn = (turn+1)%2;
 		}
-		turn = (turn+1)%2;
-	}
+		string choice;
+		cout<<"\n\nDo you want to play again? (y/n) ";
+		cout<<"\nPress any other key to exit: ";
+		getline(cin,choice);
+		getline(cin,choice);
+		if(choice=="y"){
+			cout<<endl;
+			goto begin;	
+		}
 }
